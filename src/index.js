d2e466e059c3886bb0f53b04d9366310ee2f3aed
Steven Fisher <steven.fisher22@gmail.com>
2019-03-22 09:52:59 -0500

    simple button working

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => (
-    <div>
-        Test
-    </div>
-)
+function handleAction(event) {
+    console.log('Child did:', event)
+}
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+function Parent() {
+    return (
+        <Child onAction={handleAction} />
+    )
+}
+
+function Child({onAction}) {
+    return (
+        <button onClick={onAction}>
+            Click Me!
+        </button>
+    )
+}
+
+ReactDOM.render(<Parent />, document.querySelector('#root'))
